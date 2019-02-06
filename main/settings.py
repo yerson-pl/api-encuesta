86254b9c4825ca4286252747551ac429d5e20915
yerson <yerson.pl@gmail.com>
2019-02-06 16:02:09 -0500

    nuevas conf

--- main/settings.py
+++ main/settings.py
@@ -39,13 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'apps.encuestas',
 ]
 
+# Config of Corsheaders Middeware
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # adding
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_ALLOW_ALL = True  # False
+
+CORS_ORIGIN_WHITELIST = ('127.0.0.1:8080', 'localhost:8080')
